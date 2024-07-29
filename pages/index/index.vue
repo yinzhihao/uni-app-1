d2c696f34342e7d0f983d4388d0956df6560e558
yzh115 <yzh115@zcsmart.com>
2024-07-29 14:07:11 +0800

    卡片

---
+++ pages/index/index.vue
@@ -0,0 +1,40 @@
+<template>
+	<view class="page">
+		<view class="card">
+			<view class="logo-wraper">
+				<image src="https://gd-hbimg.huaban.com/b8567739b7740b7ef4bcd59db8b4d022aefd9cbf5f02-RtIkc0_fw1200" mode="widthFix" class="logo"></image>
+			</view>
+			<view class="info-wraper">
+				<view class="name">
+					你的名字
+				</view>
+				<view class="office">
+					iOS开发工程师
+				</view>
+				<view class="email">
+					abc@fawfa.com
+				</view>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				title: 'Hello'
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+	@import url("index.css");
+</style>
